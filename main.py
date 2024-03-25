9090d67e688574a75d1515e0f7b1a96f0847f79f
Luke Thomas <lukethomasyig@gmail.com>
2024-03-25 22:41:27 +0000

    First Commit

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print("I'm learning how to program.")
+#displayText "I'm learning how to program in Python." on RUN.
+
+print("I'm learning how to program in Python.")
+
+#displayText "That's awesome!" on RUN.
+
+print("That's awesome!")
